b8fdeed12cebc233d7b0fc01b3b479676c0bfa85
Zdeněk Krupička <31694089+MrScruf@users.noreply.github.com>
2020-08-04 09:50:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/ahoj', (req, res) => res.send('Ahojda zpátky!'))
+
+app.get('/ahoj/ahojda', (req, res) => res.send('Ahojda, jojda,Hello World!'))
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
